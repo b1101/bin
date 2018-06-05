9b216ce12a83380e447ad58bf8d62a8340cd3253
Roman Volosatovs <rvolosatovs@riseup.net>
2018-06-05 15:12:45 +0200

    git-rebase-all: Fix

--- scripts/git-rebase-all.sh
+++ scripts/git-rebase-all.sh
@@ -3,7 +3,8 @@ set -e
 
 base=${1:-master}
 for b in $(git for-each-ref --no-contains "${base}" refs/heads --format '%(refname:lstrip=2)'); do 
-    if ! git checkout -q "${b}" && git rebase -q "${base}" > /dev/null; then 
+    git checkout -q "${b}"
+    if ! git rebase -q "${base}" > /dev/null; then 
         echo "*** Failed to rebase ${b}"
         git rebase --abort
     fi
